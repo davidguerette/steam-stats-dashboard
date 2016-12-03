73170bd5bc78033304b20f88e181612750f102f8
David Guerette <david.guerette@gmail.com>
2016-12-02 20:37:52 -0500

    Game object now accepts dict of params. Adding properties to get time played for individual games.

--- steam_stats_dashboard/steam_stats_dashboard/helpers/game.py
+++ steam_stats_dashboard/steam_stats_dashboard/helpers/game.py
@@ -2,16 +2,36 @@
 Game object module
 '''
 
+from .time_calc import TimeCalc
+
 class Game:
-    def __init__(self, app_id, name, icon_img, logo_img, playtime_mins, playtime_mins_two_weeks=0):
-        self.app_id = app_id
-        self.name = name
-        self.icon_img = icon_img
-        self.logo_img = logo_img
+    ''' Game object class, representing a Steam game owned by a player '''
+    def __init__(self, game_dict, playtime_mins_two_weeks):
+        ''' Instantiate game object
+            @param game_dict: data for an individual game, from full game json response
+            @param int playtime_mins_two_weeks: number of mins the game has been played by
+            player over the past two weeks
+        '''
+        self.app_id = game_dict['appid']
+        self.name = game_dict['name']
+        self.icon_img = game_dict['img_icon_url']
+        self.logo_img = game_dict['img_logo_url']
 
         # Player-specific fields
-        self.playtime_mins = playtime_mins
+        self.playtime_mins = game_dict['playtime_forever']
         self.playtime_mins_two_weeks = playtime_mins_two_weeks
 
     def __repr__(self):
         return "{0} - App ID: {1}".format(self.name, self.app_id)
+
+    @property
+    def time_played_total_dict(self):
+        ''' Return time dict for amount of time spent in this game
+            Possible time dict keys: 'years', 'weeks', 'days', 'hours', 'minutes'
+        '''
+        return TimeCalc.mins_to_time_dict(self.playtime_mins)
+
+    @property
+    def time_played_total_hours(self):
+        ''' Convert number of mins game has been played from mins to hours '''
+        return TimeCalc.hours_from_minutes(self.playtime_mins)
