8e7445b6216770a54b81f97768b69c00758ae205
David Guerette <david.guerette@gmail.com>
2016-12-02 20:35:55 -0500

    Modifying mins_to_time_dict() to use divmod for getting mins remainder

--- steam_stats_dashboard/steam_stats_dashboard/helpers/time_calc.py
+++ steam_stats_dashboard/steam_stats_dashboard/helpers/time_calc.py
@@ -4,12 +4,15 @@ Module to handle calculation of time stat metrics
 import time
 import datetime
 
-MINS_PER_YEAR = 524160
-MINS_PER_WEEK = 10080
-MINS_PER_DAY = 1440
-MINS_PER_HOUR = 60
 SECONDS_PER_MINUTE = 60
+MINS_PER_HOUR = 60
 HOURS_PER_DAY = 24
+DAYS_PER_WEEK = 7
+DAYS_PER_YEAR = 365.25
+
+MINS_PER_DAY = MINS_PER_HOUR * HOURS_PER_DAY
+MINS_PER_WEEK = MINS_PER_DAY * DAYS_PER_WEEK
+MINS_PER_YEAR = MINS_PER_DAY * DAYS_PER_YEAR
 
 class TimeCalc:
     ''' Utility class for time conversion methods '''
@@ -17,48 +20,58 @@ class TimeCalc:
     two_weeks_ago_time = time.time() - datetime.timedelta(weeks=2).total_seconds()
 
     @staticmethod
-    def mins_to_time_dict(mins_played):
+    def mins_to_time_dict(total_mins_played):
         ''' Create dict of years, weeks, days, hours, and minutes from minutes
-            @param int mins_played: total number of minutes played
+            @param int total_mins_played: total number of minutes played
             @return dict time_played_dict
         '''
         time_played_dict = {}
+        mins_remainder = total_mins_played
 
-        while mins_played:
-            if mins_played >= MINS_PER_YEAR:
-                years = int(mins_played / MINS_PER_YEAR)
-                time_played_dict['years'] = years
-                mins_played -= years * MINS_PER_YEAR
-            elif MINS_PER_WEEK <= mins_played < MINS_PER_YEAR:
-                weeks = int(mins_played / MINS_PER_WEEK)
-                time_played_dict['weeks'] = weeks
-                mins_played -= weeks * MINS_PER_WEEK
-            elif MINS_PER_DAY <= mins_played < MINS_PER_WEEK:
-                days = int(mins_played / MINS_PER_DAY)
-                time_played_dict['days'] = days
-                mins_played -= days * MINS_PER_DAY
-            elif MINS_PER_HOUR <= mins_played < MINS_PER_DAY:
-                hours = int(mins_played / MINS_PER_HOUR)
-                time_played_dict['hours'] = hours
-                mins_played -= hours * MINS_PER_HOUR
-            else:
-                minutes = int(mins_played)
-                time_played_dict['minutes'] = minutes
-                mins_played -= minutes
+        # Years
+        time_value, mins_remainder = divmod(mins_remainder, MINS_PER_YEAR)
+        if time_value:
+            time_played_dict['years'] = int(time_value)
+
+        # Weeks
+        time_value, mins_remainder = divmod(mins_remainder, MINS_PER_WEEK)
+        if time_value:
+            time_played_dict['weeks'] = int(time_value)
+
+        # Days
+        time_value, mins_remainder = divmod(mins_remainder, MINS_PER_DAY)
+        if time_value:
+            time_played_dict['days'] = int(time_value)
+
+        # Hours
+        time_value, mins_remainder = divmod(mins_remainder, MINS_PER_HOUR)
+        if time_value:
+            time_played_dict['hours'] = int(time_value)
+
+        # Minutes (remainder)
+        time_played_dict['minutes'] = int(mins_remainder)
 
         return time_played_dict
 
     @staticmethod
-    def avg_time_per_day(mins_played, start_time):
+    def hours_from_minutes(mins_played):
+        ''' Return number of hours from mins, rounded to one decimal place '''
+        if mins_played:
+            return round(mins_played / MINS_PER_HOUR, 1)
+        else:
+            return None
+
+    @staticmethod
+    def avg_mins_per_day(mins_played, start_time):
         ''' Return time dict containing average number of minutes or hours played
             per day since a provided start time. Round to the nearest minute.
             @param int mins_played: number of minutes played since the start time
-            @param int start_time: epoch time in seconds to use as starting point for calculating average
+            @param int start_time: epoch time to use as starting point for calculating average
         '''
         if mins_played:
-            days_since_start_time = int(time.time() - start_time) / SECONDS_PER_MINUTE / MINS_PER_HOUR / HOURS_PER_DAY
-            avg_mins_per_day = round(mins_played / days_since_start_time)
+            days_since_start_time = (time.time() - start_time) / SECONDS_PER_MINUTE / MINS_PER_DAY
+            avg_mins_per_day = mins_played / days_since_start_time
         else:
             avg_mins_per_day = 0
 
-        return TimeCalc.mins_to_time_dict(avg_mins_per_day)
+        return avg_mins_per_day
